519eca10984a1ca1873da90f1b7f7fdd7bce724f
masharelli <a01633090@itesm.mx>
2021-12-31 20:50:21 -0600

    Insertion sort algorithm

---
+++ insertionSort.py
@@ -0,0 +1,23 @@
+def insertionSort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+
+def insertionSortDecrease(arr):
+    for i in range(1, len(arr)):
+        key = arr [i]
+        j = i - 1
+        while j >= 0 and  key > arr[j]:
+            arr [j + 1] = arr[j]
+            j = j - 1 
+        arr [j + 1] = key
+
+arr = [5,2,4,6,1,3]
+insertionSort(arr)
+for i in range(len(arr)):
+    print("%d" %arr[i])
+
